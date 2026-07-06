ec20d8482bb8c94d525cbc710477cd654ee8c45f
_satyam <satyam3396.beai24@chitkara.edu.in>
2026-07-06 13:49:41 +0530

    Update SubstringsofSizeThreewithDistinctCharacters.java

--- Strings/SubstringsofSizeThreewithDistinctCharacters.java
+++ Strings/SubstringsofSizeThreewithDistinctCharacters.java
@@ -14,9 +14,47 @@ class Solution {
     }
 }
 
+// -------------------------------------------------------------------------------------------------
 
+// Also when to try this approch of taking characters like in this ques we did - 
 
-// key mistake here - 
+// Yahan Sliding Window ki zarurat kyu nahi padi?
+// Kyuki har substring ke liye sirf 3 comparisons karne the.
+    
+// Ab Second Question (1456)
+// Question:
+// Find maximum vowels in every substring of length k.
+
+// Ab main tumse ek question puchhta hoon.
+// Case 1
+// Window 1
+// abc
+// Next
+// bcd
+// Question hai:
+// "Are all characters different?"
+// Kya tum sirf a aur d dekh kar answer bata sakte ho?
+// Nahi.
+// Kyuki b aur c bhi compare karne padenge.
+// Case 2
+// Window
+// abc
+// Question hai
+// "Kitne vowels hain?"
+// Pehle answer tha
+// 1 vowel
+// Next
+// bcd
+// Kya tum sirf
+// a gaya
+// d aaya
+// dekh kar answer nikal sakte ho?
+// Haan.
+// Kyuki b aur c waise ke waise hi hain.
+
+// --------------------------------------------------------------------------------------------------    
+    
+    // key mistake here - 
 //   if(first != second != third) wali condition apply krdi thi mene yaha to wo galat h bcoz - 
 //   The expression is evaluated from left to right.
 //   so java - 
