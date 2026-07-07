4aada0fb535eb1651569df694d28dfc9d040009f
_satyam <satyam3396.beai24@chitkara.edu.in>
2026-07-07 17:28:06 +0530

    Update FindPairsWithGivenSum.java

--- LinkedList/FindPairsWithGivenSum.java
+++ LinkedList/FindPairsWithGivenSum.java
@@ -1,31 +1,24 @@
 class FindPairsWithGivenSum {
     public static ArrayList<ArrayList<Integer>> findPairsWithGivenSum(int target, Node head) {
-
         ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
-
         if(head == null) return ans;
 
         Node left = head;
         Node right = head;
-
         // find tail
         while(right.next != null)
         {
             right = right.next;
         }
-
         while(left != right && right.next != left)
         {
             int sum = left.data + right.data;
-
             if(sum == target)
             {
                 ArrayList<Integer> pair = new ArrayList<>();
                 pair.add(left.data);
                 pair.add(right.data);
-
                 ans.add(pair);
-
                 left = left.next;
                 right = right.prev;
             }
@@ -38,7 +31,33 @@ class FindPairsWithGivenSum {
                 right = right.prev;
             }
         }
+        return ans;
+    }
+}
 
+
+
+
+// Bruteee approch
+
+
+class Solution {
+    public ArrayList<ArrayList<Integer>> findPairsWithGivenSum(int target, Node head) {
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+        Node first = head;
+        while (first != null) {
+            Node second = first.next;
+            while (second != null) {
+                if (first.data + second.data == target) {
+                    ArrayList<Integer> pair = new ArrayList<>();
+                    pair.add(first.data);
+                    pair.add(second.data);
+                    ans.add(pair);
+                }
+                second = second.next;
+            }
+            first = first.next;
+        }
         return ans;
     }
 }
