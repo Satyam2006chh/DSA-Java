6b7e1735f22063473ebdc4ce2bedf1313cc0d33f
_satyam <satyam3396.beai24@chitkara.edu.in>
2026-07-07 17:37:56 +0530

    Add brute force method to delete middle node
    Implemented a brute force approach to delete the middle node from a linked list by counting nodes and adjusting pointers.

--- LinkedList/DeleteMiddleNode.java
+++ LinkedList/DeleteMiddleNode.java
@@ -18,3 +18,34 @@ class DeleteMiddleNode {
         return  head;
     }
 }
+
+
+
+// brute approch - 
+
+class Solution {
+    public ListNode deleteMiddle(ListNode head) {
+        // If there is only one node
+        if (head == null || head.next == null) {
+            return null;
+        }
+        // Count total nodes
+        int count = 0;
+        ListNode temp = head;
+        while (temp != null) {
+            count++;
+            temp = temp.next;
+        }
+        // Find middle index
+        int middle = count / 2;
+        temp = head;
+        // Move to the node before the middle node
+        for (int i = 1; i < middle; i++) {
+            temp = temp.next;
+        }
+
+        // Delete middle node
+        temp.next = temp.next.next;
+        return head;
+    }
+}
